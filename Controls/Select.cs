17f696ee5f143697462aea1b51860f3e90db974b
HEXACTA\fmarc <fmarc@hexacta.com>
2023-03-06 14:07:53 -0300

    git add select options

---
+++ Controls/Select.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace Practice_Exam
+{
+    public class Select : BaseControl
+    {
+        public Select()
+        {
+        }
+
+        public Select(By locator)
+            : base(locator)
+        { }
+
+        private SelectElement DropSelect => new SelectElement(this.WebElement);
+
+        public void SelectByIndex(int optionIndex)
+        {
+            this.DropSelect.SelectByIndex(optionIndex);
+        }
+
+        public void SelectText(string optionText)
+        {
+            this.DropSelect.SelectByText(optionText);
+        }
+
+        public void SelectValue(string optionValue)
+        {
+            this.DropSelect.SelectByValue(optionValue);
+        }
+
+        public string SelectedOption()
+        {
+            return this.DropSelect.SelectedOption.Text;
+        }
+
+        public IEnumerable<string> Options()
+        {
+            return this.DropSelect.Options.Select(e => e.Text);
+        }
+    }
+}
